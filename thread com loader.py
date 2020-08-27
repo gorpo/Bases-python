7160e1440f5d810603be51dd692ce0b97e52a4e6
GorpoOrko <47964672+gorpo@users.noreply.github.com>
2020-08-27 14:54:55 -0300

    Add files via upload

---
+++ thread com loader.py
@@ -0,0 +1,22 @@
+import itertools
+import threading
+import time
+import sys
+
+done = False
+#here is the animation
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+        sys.stdout.write('\rloading ' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('\rDone!     ')
+
+t = threading.Thread(target=animate)
+t.start()
+
+#long process here
+time.sleep(10)
+done = True
